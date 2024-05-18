cc7f6a02ea44178efdbb63f42ad5db3aeef9ad41
Abel Wendmu <abelwendmu124@gmail.com>
2024-05-18 03:17:06 +0300

    new push

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-dynamic/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
